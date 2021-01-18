61b5913a59d771b5ce0448a3eb716481aec1009e
Sowmya-AK <sowmyaak04@gmail.com>
2021-01-18 19:37:48 +0530

    Add chain properly

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var bird, slingShot;
+var bird, slingShot,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
